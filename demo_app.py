aaa4986a54cde7a6d147ddedb7f98a05b09f9621
Mega-Gorilla <hahahadesu@gmail.com>
2024-03-24 21:04:32 +0900

    byteデータでなくダウンロードURLを返すようん修正

--- demo_app.py
+++ demo_app.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
 
 import json
+from modules.backblaze_api import upload_byte
 
 from modules.arxiv_api import get_arxiv_info_async,download_arxiv_pdf
 from modules.translate import pdf_translate
@@ -30,8 +31,9 @@ async def process_translate_arxiv_pdf(key,target_lang, arxiv_id,api_url):
         
         #翻訳処理
         translate_data = await pdf_translate(key,pdf_data,to_lang=target_lang,api_url=api_url)
-        
-        return translate_data
+        download_url = await upload_byte(translate_data, 'arxiv_pdf', F"{arxiv_id}_{target_lang}.pdf", content_type='application/pdf')
+
+        return download_url
     
     except Exception as e:
         print(f"Error processing arxiv_id {arxiv_id}: {str(e)}")
@@ -65,9 +67,9 @@ async def translate_paper_data(arxiv_id: str,deepl_url:str, deepl_key: str, targ
         if not license_ok:
             raise HTTPException(status_code=400, detail="License not permitted for translation")
         
-        translate_pdf_data = await process_translate_arxiv_pdf(deepl_key,target_lang, arxiv_id,deepl_url)
+        pdf_dl_url = await process_translate_arxiv_pdf(deepl_key,target_lang, arxiv_id,deepl_url)
             
-        return translate_pdf_data
+        return pdf_dl_url
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
